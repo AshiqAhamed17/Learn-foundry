e16a32d794b950846dc7473cd447655a2340018b
Ashiq Ahamed <aashiq121205@gmail.com>
2025-01-21 21:38:49 +0530

    Test Whale.t.sol

---
+++ FoundryBasics/test/Fuzz.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+ 
+import {Bit} from "../src/Bit.sol";
+
+contract FuzzTest is Test {
+    Bit public b;
+
+    function setUp() public {
+        b = new Bit();
+    }
+}
---
+++ FoundryBasics/test/Whale.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract ForkTest is Test {
+    IERC20 public dai;
+
+    function setUp() public {
+        dai =  IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+    }
+
+    function testDeposit() public {
+        address ash = address(123);
+        
+
+        uint balBefore = dai.balanceOf(ash);
+        console.log("Before Balance :", balBefore);
+
+        // token, account, amount, adjust total supply
+        deal(address(dai), ash, 1e6);
+
+        uint balAfter = dai.balanceOf(ash);
+        console.log("After Balance :", balAfter);
+
+        assertEq(dai.balanceOf(ash), balBefore +balAfter);
+
+    }
+
+}
+
+
