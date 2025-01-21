b59a11611f692fcefd85e7c24c1f15dbf4aeff0b
Ashiq Ahamed <aashiq121205@gmail.com>
2025-01-22 04:03:40 +0530

    invariants

---
+++ FoundryBasics/test/invariants/Invariant_0.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+// Topics
+// - Invariant
+// - Difference between fuzz and invariant
+// - Failing invariant
+// - Passing invariant
+// - Stats - runs, calls, reverts
+
+contract InvariantIntro {
+    bool public flag;
+
+    function flag_1() external {}
+    function flag_2() external {}
+    function flag_3() external {}
+    function flag_4() external {}
+    function flag_5() external {
+        flag = true;
+    }
+}
+
+contract InvariantIntroTest is Test {
+    InvariantIntro private target;
+
+    function setUp() public {
+        target = new InvariantIntro();
+    }
+
+    function invariant_flag_is_always_false() public view {
+        assertEq(target.flag(), false, "Flag is true somewhere");
+    }
+}
