c0330bb524fd9ecef0027882a177a648abd6db45
Ashiq Ahamed <aashiq121205@gmail.com>
2025-01-22 06:08:42 +0530

    Update WETH.sol

---
+++ FoundryBasics/test/invariants/Invariant_1.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import {WETH} from "../../src/WETH.sol";
+
+// NOTE: open testing - randomly call all public functions
+contract WETH_Open_Invariant_Tests is Test {
+    WETH public weth;
+
+    function setUp() public {
+        weth = new WETH();
+    }
+
+    receive() external payable {}
+
+    // NOTE: - calls = runs x depth, (runs, calls, reverts)
+    function invariant_totalSupply_is_always_zero() view public {
+        assertEq(0, weth.totalSupply());
+    }
+}
---
+++ FoundryBasics/test/invariants/Invariant_2.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import {WETH} from "../../src/WETH.sol";
+
+// Topics
+// - handler based testing - test functions under specific conditions
+// - target contract
+// - target selector
