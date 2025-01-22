6b0196cfdf0b8fc38857394a0192073601400965
Ashiq Ahamed <aashiq121205@gmail.com>
2025-01-22 21:36:10 +0530

    Invariant_2.t.sol

--- FoundryBasics/test/invariants/Invariant_2.t.sol
+++ FoundryBasics/test/invariants/Invariant_2.t.sol
@@ -9,3 +9,48 @@ import {WETH} from "../../src/WETH.sol";
 // - handler based testing - test functions under specific conditions
 // - target contract
 // - target selector
+
+import {CommonBase} from "forge-std/Base.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
+import {StdUtils} from "forge-std/StdUtils.sol";
+
+contract Handler is CommonBase, StdUtils, StdCheats {
+    WETH public weth;
+    uint256 public wethBalance;
+    uint256 public numCalls;
+
+    constructor(WETH _weth) {
+        weth = _weth;
+    }
+
+    function sendToFallback(uint _amount) public {
+        _amount = bound(_amount, 0, address(this).balance);
+
+        wethBalance += _amount;
+        numCalls++;
+        (bool success,) = address(weth).call{value: _amount}("");
+        require(success, "Failed to send to fallback");
+    }
+
+    function deposit(uint _amount) public {
+        _amount = bound(_amount, 0, address(this).balance);
+
+        wethBalance += _amount;
+        numCalls++;
+
+        weth.deposit{value: _amount}();
+    }
+
+    function withdraw(uint _amount) public {
+
+        _amount = bound(_amount, 0, weth.balanceOf(address(this)));
+        wethBalance -= _amount;
+        numCalls++;
+        weth.withdraw(_amount);
+    }
+
+    //Function to just fail the test 
+    function fail() public pure {
+        revert("Failed");
+    }
+}
